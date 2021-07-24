0624d0a1ead0c7f3e705a2c40157a4243e602cb0
stutir1412 <55341741+stutir1412@users.noreply.github.com>
2021-07-24 17:44:44 +0800

    Update Scoop_original_v2.ino

--- Code/Scoop_original_v2.ino
+++ Code/Scoop_original_v2.ino
@@ -1,9 +1,9 @@
 #include <Servo.h>
-#define LED 13 // LED connected to PIN 13
+
 
 //Creating Servo Object
 Servo spoonservo;  //D3 Spoon servo
-Servo firstservo; //D6 First servo
+Servo firstservo; //D6 First servo 5 now
 
 
 int spoonpos;
@@ -19,7 +19,7 @@ void setup() {
   spoonservo.attach(3);    
   firstservo.attach(6);  
    
-  pinMode(LED, OUTPUT); //LED 
+  //pinMode(LED, OUTPUT); //LED 
   Serial.begin(9600);
   inputString.reserve(200);
 
@@ -49,67 +49,34 @@ void loop() {
     inputString = "";    
     stringComplete = false;    
   }
- /*
-  //SpoonPos = map(0, 35, 25, 0, 75, 125, 35)
-  //FirstPos = map(130, 170, 180, 170, 115, 115, 170)
-  
-  for (pos = 30; pos <= 150; pos += 1) { 
-    Serial.println(pos);
-    spoonservo.write(pos);    
-    firstservo.write(pos);
-    delay(300);                       
-  }
-  for (pos = 150; pos >= 150; pos -= 1) { 
-    Serial.println(pos);
-    spoonservo.write(pos);   
-    firstservo.write(pos);
-    delay(300);               
-  } 
-}
-
-void feed(){ */
-  //Home Position
-  Serial.print("start");
-  spoonservo.write(0);
-  firstservo.write(130);
-  delay (1000);
-  
-  //Feed start -Home
-  Serial.print("home");
-  spoonservo.write(35);
-  firstservo.write(170);
-  delay (1000); 
-   
-   // Feed -1
-  Serial.print("feed position 1");
-  spoonservo.write(25);
-  firstservo.write(180);
-  delay (1000);   
-
-  // Feed-2 
-  Serial.print("feed position 2");
-  spoonservo.write(0);
-  firstservo.write(170);
-  delay (1000);   
-
-  //Feed -Position
-  Serial.print("feed position final");
-  spoonservo.write(75);
-  firstservo.write(115);
-  delay (1000);  
-
-  //Feed - Reverse -1
-  Serial.print("feed reverse");
-  spoonservo.write(125);
-  firstservo.write(115);
-  delay (1000); 
-
-  //Feed start-Home
-  spoonservo.write(35);
-  firstservo.write(170);
-   Serial.println("End");
-  delay (1000); 
+ //Feed Process
  
+
+//start
+  Serial.print("step1");
+  firstservo.write(75);
+  spoonservo.write(100);
+  delay (1000); 
+  
+  Serial.print("step2");
+  firstservo.write(70);
+  spoonservo.write(90);
+  delay (1000); 
+
+  Serial.print("step3");
+  firstservo.write(60);
+  spoonservo.write(60);
+  delay (1000);
+
+  Serial.print("step4");
+  firstservo.write(30);
+  spoonservo.write(40);
+  delay (1000);
+//feed position
+  Serial.print("step5");
+  firstservo.write(0);
+   spoonservo.write(0);
+  delay (5000);
 }
 
 
