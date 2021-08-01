81fc46bb0444dd448ffbe4ad789bb7a1d30da606
stutir1412 <55341741+stutir1412@users.noreply.github.com>
2021-08-01 16:56:02 +0800

    Update Scoop_original_v2.ino

--- Code/Scoop_original_v2.ino
+++ Code/Scoop_original_v2.ino
@@ -2,8 +2,8 @@
 
 
 //Creating Servo Object
-Servo spoonservo;  //D3 Spoon servo
-Servo firstservo; //D6 First servo 5 now
+Servo spoonservo;  //D3 Spoon servo  - SG90 Micro servo (Tower Pro) ; not analog
+Servo firstservo; //D6 First servo  -TowerPro MG 996R
 
 
 int spoonpos;
@@ -49,38 +49,47 @@ void loop() {
     inputString = "";    
     stringComplete = false;    
   }
- //Feed Process
+ // Start Feed Process
+
+ Serial.print("step1"); // Rod at home position
+ firstservo.write(75);
+ delay (1000); 
  
+ Serial.print("start spoon scoop mechanism"); //Rod still at home position
+ spoonservo.write(190);  //spoon position 1
+ delay (1000); 
+ spoonservo.write(185); //spoon position 2
+ delay (1000); 
+ spoonservo.write(160); //spoon position 3
+ delay (1000); 
+ spoonservo.write(100); //spoon position 4
+ delay (1000); 
+ spoonservo.write(90);  //spoon position 5
+ delay (1000); 
+ spoonservo.write(82); //spoon feed position 
+ Serial.print("Spoon Feed position");
+ delay (8000); 
+ 
+ Serial.print("step2"); //Rod moves with spoon at feed position
+ firstservo.write(70);
 
-//start
-  Serial.print("step1");
-  firstservo.write(75);
-  spoonservo.write(100);
-  delay (1000); 
+ Serial.print("step3"); //Rod moves with spoon at feed position
+ firstservo.write(60);
+ delay (1000);
+
+ Serial.print("step4"); //Rod moves with spoon at feed position
+ firstservo.write(30);
+
+ Serial.print("step5"); //Rod and spoon both at feed position
+ firstservo.write(0);
+ delay (5000); 
   
-  Serial.print("step2");
-  firstservo.write(70);
-  spoonservo.write(90);
-  delay (1000); 
+ Serial.print("step0"); // Process restarts - Rod at home position
+ firstservo.write(75);
 
-  Serial.print("step3");
-  firstservo.write(60);
-  spoonservo.write(60);
-  delay (1000);
-
-  Serial.print("step4");
-  firstservo.write(30);
-  spoonservo.write(40);
-  delay (1000);
-//feed position
-  Serial.print("step5");
-  firstservo.write(0);
-   spoonservo.write(0);
-  delay (5000);
 }
 
-
-void serialEvent() {
+void serialEvent() {           
   while (Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read();
