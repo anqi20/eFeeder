1b87160938fbf5142a204191fc26c1aeb795bbf2
LAPTOP-9AV7NJD2\Lim An Qi <anqi20@hotmail.com>
2021-08-02 22:12:55 +0800

    Update receiver code

---
+++ Code/Receiver_switch.ino
@@ -0,0 +1,40 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define CE_PIN 9 
+#define CSN_PIN 10 
+
+const uint64_t pipe = 0xE8E8F0F0E1LL; 
+int buttonState; 
+
+RF24 radio(CE_PIN, CSN_PIN); // CE, CSN
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000); 
+  Serial.println("NRF24L01 Receiver Starting"); 
+  radio.begin();
+  radio.setDataRate(RF24_250KBPS);
+  radio.openReadingPipe(1, pipe);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+
+void loop() {
+  /*if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  } else {
+    Serial.println("No radio available"); 
+  }*/
+
+  if(radio.available()) {
+    radio.read(&buttonState, sizeof(buttonState)); 
+    if(buttonState == HIGH) {
+      Serial.println("Button has been pressed!!");
+      delay(500);
+    }
+  }
+}
